cc2293642c8f473e12d0dff6df16f45ae0e98d39
Brandon Corfman <brandon.c.corfman.civ@mail.mil>
2021-10-04 23:10:12 -0400

    FIX: change setup and teardown to use \Users\brandon.corfman\Desktop for env

--- cicd/setup_conda_env.bat
+++ cicd/setup_conda_env.bat
@@ -1,2 +1,2 @@
-call conda create -y -n vtk -c conda-forge python==3.9.* nuitka vtk
-call conda activate vtk
+call conda create -y -n %HOMEPATH%\Desktop\vtk-env -c conda-forge python==3.9.* nuitka vtk
+call conda activate %HOMEPATH%\Desktop\vtk-env
--- cicd/teardown_conda_env.bat
+++ cicd/teardown_conda_env.bat
@@ -1,2 +1,4 @@
 call conda deactivate 
-call conda remove -y -n vtk --all
+call conda remove -y -p %HOMEPATH%\Desktop\vtk-env --all
+call rmdir /s /q %HOMEPATH%\Desktop\vtk-env
+call conda env list
